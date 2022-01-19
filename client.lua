6a9bac635eea8e844978fcaa1f354c24772a066b
yamieboi <90822795+yamieboi@users.noreply.github.com>
2022-01-20 01:41:11 +0530

    so far very good

--- client.lua
+++ client.lua
@@ -1,6 +1,9 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local cache = {}
 local Proximity = nil
+local in_progress = false
+local DICT = nil
+local ANIM = nil
 local function DrawText3D(x, y, z, text)
     SetTextScale(0.35, 0.35)
     SetTextFont(4)
@@ -26,20 +29,57 @@ local function GetClosestPlantloop()
     end
     return Proximity
 end
-
+local function ProgressBarPickup(Event,Name,Duration,AnimDict,AnimName)
+    in_progress = true
+    if AnimDict ~= nil and AnimName ~= nil then
+        DICT = AnimDict
+        ANIM = AnimName
+    end
+    QBCore.Functions.Progressbar(Name, Name, Duration, false, true, {
+        disableMovement = true,
+        disableCarMovement = true,
+        disableMouse = false,
+        disableCombat = true,
+    }, {
+        animDict = DICT,
+        anim = ANIM,
+        flags = 16,
+    }, {}, {}, function()
+       in_progress = false
+       DICT = nil
+       ANIM = nil
+       ClearPedTasksImmediately(PlayerPedId())
+       TriggerServerEvent(Event)
+    end, function()
+        in_progress = false
+        DICT = nil
+        ANIM = nil
+        ClearPedTasksImmediately(PlayerPedId())
+        QBCore.Functions.Notify("Canceled..", "error")
+    end)
+end
 --[[ AddEventHandler('QBCore:Client:OnPlayerLoaded', function() ]]
     for key, value in pairs(Cfg.Jobs) do
-        cache[#cache+1] = {['coords'] = value.pickup_coords,['text'] = value.pickup_3dtext,['trigger_name_server'] = ''..value.job_name..'Pickup' }
+        cache[#cache+1] = {
+            ['coords'] = value.pickup_coords,
+            ['text'] = value.pickup_3dtext,
+            ['trigger_name_server'] = ''..value.job_name..'Pickup',
+            ['pickup_progress_name'] = value.pickup_progress_name,
+            ['pickup_progress_duration'] = value.pickup_progress_duration,
+            ['pickup_progress_animation_dict'] = value.pickup_progress_animation_dict, 
+            ['pickup_progress_animation_name'] = value.pickup_progress_animation_name,
+
+        }
     end
 --[[ end) ]]
 CreateThread(function()
     while true do
         Wait(2)
         local ProximityL = GetClosestPlantloop()
-        if ProximityL ~= nil and #(ProximityL.coords - GetEntityCoords(PlayerPedId()) ) <= 2  and not IsPedInAnyVehicle(PlayerPedId(), false) then
+        if not in_progress and ProximityL ~= nil and #(ProximityL.coords - GetEntityCoords(PlayerPedId()) ) <= 2  and not IsPedInAnyVehicle(PlayerPedId(), false) then
          DrawText3D(ProximityL.coords.x, ProximityL.coords.y, ProximityL.coords.z, ProximityL.text)
          if IsControlJustPressed(0,184) then
-             TriggerServerEvent(ProximityL.trigger_name_server)
+            ProgressBarPickup(ProximityL.trigger_name_server,ProximityL.pickup_progress_name,ProximityL.pickup_progress_duration,ProximityL.pickup_progress_animation_dict,ProximityL.pickup_progress_animation_name)
          end
         else 
           Wait(2000)
--- server.lua
+++ server.lua
@@ -3,5 +3,11 @@ local QBCore = exports['qb-core']:GetCoreObject()
 for key, value in pairs(Cfg.Jobs) do
     RegisterNetEvent(''..value.job_name..'Pickup',function ()
         print('kuttar bacha')
+        if value.pickup_item_take ~= nil then
+            print(value.pickup_item_take)
+        end
+        if value.pickup_item_give ~= nil then
+            print(value.pickup_item_give)
+        end
     end)
 end

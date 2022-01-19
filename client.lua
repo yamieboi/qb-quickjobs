cf03fc032f19c17b6bfc1a9b7abebd353f7f1b65
yamieboi <90822795+yamieboi@users.noreply.github.com>
2022-01-20 00:33:39 +0530

    loop base

--- client.lua
+++ client.lua
@@ -1,14 +1,49 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local cache = {}
+local Proximity = nil
+local function DrawText3D(x, y, z, text)
+    SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry('STRING')
+    SetTextCentre(true)
+    AddTextComponentString(text)
+    SetDrawOrigin(x, y, z, 0)
+    DrawText(0.0, 0.0)
+    local factor = (string.len(text)) / 370
+    DrawRect(0.0, 0.0 + 0.0125, 0.017 + factor, 0.03, 0, 0, 0, 75)
+    ClearDrawOrigin()
+end
+local function GetClosestPlantloop()
+    local dist = 3
 
-for key, value in pairs(Cfg.Jobs) do
-    --[[ QBCore.Functions.Notify(value.pickup) ]]
-    cache[#cache+1] = value.pickup
-   
+    for i = 1, #cache do
+        local distance = #(GetEntityCoords(PlayerPedId()) - cache[i])
+        if distance < dist then
+            Proximity = cache[i]
+
+        end
+    end
+
+    return Proximity
 end
 
-for key, value in pairs(cache) do
-    QBCore.Functions.Notify(value)
-    --[[ cache[#cache+1] = value.pickup ]]
-   
-end
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+    for key, value in pairs(Cfg.Jobs) do
+        cache[#cache+1] = value.pickup_coords
+    end
+end)
+CreateThread(function()
+    while true do
+        Wait(2)
+        local ProximityL = GetClosestPlantloop()
+        if ProximityL ~= nil and #(ProximityL - GetEntityCoords(PlayerPedId()) ) <= 2  and not IsPedInAnyVehicle(PlayerPedId(), false) then
+         DrawText3D(ProximityL.x, ProximityL.y, ProximityL.z, '~r~E~w~ - Destroy Plant')
+        else 
+          Wait(2000)
+        end
+
+
+    end
+end)
--- config.lua
+++ config.lua
@@ -3,8 +3,14 @@ Cfg = {}
 Cfg.Jobs = {
     ['job1'] = {
         ['pickup'] = 'LOL XD',
+        ['pickup_coords'] = vector3(203.17, -797.37, 31.07),
     },
     ['job2'] = {
-        ['pickup'] = 'BRUH'
+        ['pickup'] = 'BRUH',
+        ['pickup_coords'] = vector3(204.27, -794.18, 31.05),
+    },
+    ['job3'] = {
+        ['pickup'] = 'BRUH',
+        ['pickup_coords'] = vector3(201.17, -802.48, 31.1),
     }
 }
